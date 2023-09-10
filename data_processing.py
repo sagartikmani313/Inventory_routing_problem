0164cef18f8eb5336fc83b1e7197ec8dfd11e90f
oyesagarsun22 <sagartikmani900@gmail.com>
2023-09-10 13:18:11 +0100

    final changes

--- data_processing.py
+++ data_processing.py
@@ -1,9 +1,10 @@
 import pandas as pd
 import numpy as np
-# import time
-# import plant_supplier_distances as psd
 
 def get_cluster_file(filename,sheetname):
+
+    '''This function gets the cluster file from the excel sheet and returns it in the form of a dataframe'''
+
     clusters = pd.read_excel(filename,sheet_name=sheetname)
     clusters.drop(clusters.iloc[:,5:],inplace=True,axis=1)
     clusters.fillna(0,inplace=True)
@@ -11,6 +12,9 @@ def get_cluster_file(filename,sheetname):
     print("Cluster file processed")
     return clusters
 def get_supplier_file(filename,sheetname):
+
+    '''This function gets the supplier file from the excel sheet and returns it in the form of a dataframe'''
+
     suppliers = pd.read_excel(filename,sheet_name=sheetname)
     suppliers.fillna(0,inplace=True)
     suppliers = suppliers.astype(np.int64)
@@ -25,6 +29,9 @@ supplier_file, supplier_sublist = get_supplier_file("region_3_compiled.xlsx","su
 # filling the matrix with ones where the supplier is present in the cluster
 
 def fill_matrix(no_of_suppliers,cluster_id,number_of_clusters):
+
+    '''This function fills the matrix with ones where the supplier is present in the cluster'''
+
     #initialising the matrix
     mat = np.zeros([no_of_suppliers,number_of_clusters],dtype=int)
     for i in range(no_of_suppliers):
